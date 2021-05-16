382a329fd1ccbc1ef602738a094998db3fbf0bde
Bandi72006 <bandi72006@gmail.com>
2021-05-16 12:28:50 +0400

    You can now convert text into a cipher (image)

--- main.py
+++ main.py
@@ -8,10 +8,29 @@ for i in range(255):
 mode = int(input("Enter mode:  \n 0 = create, 1 = decipher \n"))
 
 if mode == 0: #Create
-    print("Work in progress")
+    x = 0
+    y = 0
+    message = input("Enter message:   ")
+    image = Image.new("RGB", (10,10), color = (32,32,32))
+    image.save("image/696d616765546f436f6e766572740a.png")
 
-elif mode == 1:
-    image = Image.open("image/696d616765546f436f6e766572740a.jpg")
+    image = Image.open("image/696d616765546f436f6e766572740a.png")
+    picture = image.load()
+
+    RGBTuple = []
+    for char in message:
+        if len(RGBTuple) == 3:
+            RGBTuple = tuple(RGBTuple)
+            picture[x,y] = RGBTuple
+            RGBTuple = []
+            x += 1
+
+        RGBTuple.append(chars.index(char))
+
+    image.save("image/696d616765546f436f6e766572740a.png")
+
+elif mode == 1: #Decipher
+    image = Image.open("image/696d616765546f436f6e766572740a.png")
     picture = image.load()
 
     size = image.size
