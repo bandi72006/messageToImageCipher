685871022eb12e68f212949d1115d7ecd2c6d3d7
Bandi72006 <bandi72006@gmail.com>
2021-05-16 12:05:47 +0400

    Finished image decoder

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from PIL import Image
 
-image = Image.open("image/696d616765546f436f6e766572740a.jpg")
-picture = image.load()
+#Creates list with all ASCII characters
+chars = []
+for i in range(255):
+    chars.append(chr(i))
 
-size = image.size
+mode = int(input("Enter mode:  \n 0 = create, 1 = decipher \n"))
 
-values = []
+if mode == 0: #Create
+    print("Work in progress")
 
-for j in range(size[1]):
-    for i in range(size[0]):
-        values.append(picture[i, j])
+elif mode == 1:
+    image = Image.open("image/696d616765546f436f6e766572740a.jpg")
+    picture = image.load()
 
+    size = image.size
+
+    values = []
+
+    for j in range(size[1]):
+        for i in range(size[0]):
+            values.append(picture[i, j])
+
+    message = []
+    for colour in values:
+        for RGB in colour:
+            message.append(chars[RGB])
+    
+    message = "".join(message)
+    print(message)
